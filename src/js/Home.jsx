da1d05bae6bbdb9d4e29bdea7fad9b05627b34f2
Ludivine <35698784+Ludivine-WhitFlarr@users.noreply.github.com>
2018-10-02 11:21:31 +0200

    galerie

--- src/js/Home.jsx
+++ src/js/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Article from './Article';
+import Galerie from './Galerie';
 
 export default class Home extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Home extends Component {
               <h2>Présentation</h2>
               <p>Ceci est un paragraphe extrement long car se doit etre un article mais je ne sais pas quoi mettre donc j'ecrit des mot qui me vienne en faisant des phrase qui essaye d'avoir un sens mais bon je ne peut rien promettre car c'est très compliquer donc je fais de mon meieux pour pas faire de faute mais j'ai pas mis de ponctuation bonne chance pour lire se paragraphe ultra long je sais pas quoi mettre donc je tourne en rond tous sa pour dire que j ecrit un paragraphe ultra long car c'est censer etre un article bonne chan ce pour lire touts sa car sa fait deja deux ligne de code j'ai fini mon dernier mot sera carotte donc voila carotte Ceci est un paragraphe extrement long car se doit etre un article mais je ne sais pas quoi mettre donc j'ecrit des mot qui me vienne en faisant des phrase qui essaye d'avoir un sens mais bon je ne peut rien promettre car c'est très compliquer donc je fais de mon meieux pour pas faire de faute mais j'ai pas mis de ponctuation bonne chance pour lire se paragraphe ultra long je sais pas quoi mettre donc je tourne en rond tous sa pour dire que j ecrit un paragraphe ultra long car c'est censer etre un article bonne chan ce pour lire touts sa car sa fait deja deux ligne de code j'ai fini mon dernier mot sera carotte donc voila carotte </p>
             </section>
+            <Galerie />
             <Article />
           </Fragment>
     );
